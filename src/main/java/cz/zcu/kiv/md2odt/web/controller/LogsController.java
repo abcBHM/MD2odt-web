6c26fd73b3a3e1721684cc17ffb03a7cacbe9ab6
Patrik Harag <patrikharag@gmail.com>
2017-04-21 19:48:54 +0200

    Add filtering by log id

--- src/main/java/cz/zcu/kiv/md2odt/web/controller/LogsController.java
+++ src/main/java/cz/zcu/kiv/md2odt/web/controller/LogsController.java
@@ -5,31 +5,55 @@ import cz.zcu.kiv.md2odt.web.service.LogStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
- * @version 2017-04-16
+ * @version 2017-04-21
  * @author Patrik Harag
  */
 @Controller
 public class LogsController {
 
+    private static final String JSON = "application/json; charset=UTF-8";
+
     @Autowired
     private LogStorage logStorage;
 
-    @RequestMapping(value = {"/logs-raw"}, produces={"application/json; charset=UTF-8"})
+    // JSON
+
+    @RequestMapping(value = "/data/logs", produces = JSON)
     @ResponseBody
-    public List<LogEntry> logsRaw(Model model) {
+    public List<LogEntry> logsRaw() {
         return logStorage.getAll();
     }
 
-    @RequestMapping(value = {"/logs"})
+    @RequestMapping(value = "/data/logs/{id}", produces = JSON)
+    @ResponseBody
+    public List<LogEntry> logsRaw(@PathVariable int id) {
+        // TODO: add getById to LogStorage
+        return logStorage.getAll().stream()
+                .filter(e -> e.getId() == id)
+                .collect(Collectors.toList());
+    }
+
+    // HTML
+
+    @RequestMapping(value = "/logs")
     public String logs(Model model) {
-        model.addAttribute("entries", logStorage.getAll());
+        model.addAttribute("entries", logsRaw());
+
+        return "logs";
+    }
+
+    @RequestMapping(value = "/logs/{id}")
+    public String logs(Model model, @PathVariable int id) {
+        model.addAttribute("entries", logsRaw(id));
 
         return "logs";
     }
